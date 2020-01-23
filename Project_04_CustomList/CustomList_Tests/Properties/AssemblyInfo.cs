0524dcd8dc22e0a1358a084b0283ce08f30f5da3
Anthony Gomez <gomeztony004@gmail.com>
2020-01-23 11:27:01 -0600

    Created Test class in CustomList project and created first test method (with no logic) to beging to verify that it will add a value to the zero index of my custom list

---
+++ Project_04_CustomList/CustomList_Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CustomList_Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CustomList_Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("15bb5af2-2f11-4ca6-954c-7e5d91efa159")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Project_04_CustomList/CustomList_Tests/Test_AddMethod.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Project_04_CustomList;
+
+
+namespace CustomList_Tests
+{
+    [TestClass]
+    public class Test_AddMethod
+    {
+        [TestMethod]
+        public void CheckZeroIndex_Add()
+        {
+
+        }
+    }
+}
