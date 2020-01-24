f78a4c0b2f86d6a4213cc15b1c3cb69995149cd8
Anthony Gomez <gomeztony004@gmail.com>
2020-01-24 13:49:33 -0600

    completed minimum 5 Add tests, began putting logic for Add method in CustomLists class

--- Project_04_CustomList/Project_04_CustomList/CustomList.cs
+++ Project_04_CustomList/Project_04_CustomList/CustomList.cs
@@ -10,6 +10,31 @@ namespace Project_04_CustomList
     {
         public void Add(T item)
         {
+            // VARIABLES (HAS A...)
+            int count = 1;
+            int capacity = 4;
+                        
+            T[] items = new T[4];
+            T[] temp = new T[capacity * 2];
+
+            
+
+            // CONSTRUCTOR
+            public CustomList ()
+            {
+
+            }
+
+            // METHODS (CAN DO...)
+            for (int i = 0; i < count; i++)
+            {
+                temp[i] = items[i];
+                count++;
+                if (i == count || i == capacity) ;
+                {
+                    continue;
+                }
+            }
 
         }
     }
