02360bcfde64a89213339ebbc4858b110f416263
Anthony Gomez <gomeztony004@gmail.com>
2020-01-24 16:49:26 -0600

    added variables and trying to add indexer for add method

--- Project_04_CustomList/Project_04_CustomList/CustomList.cs
+++ Project_04_CustomList/Project_04_CustomList/CustomList.cs
@@ -8,34 +8,59 @@ namespace Project_04_CustomList
 {
     public class CustomList<T>
     {
-        public void Add(T item)
+        // -------------------------------------------------------------------------------------------------VARIABLES (HAS A...)----------------------------------------------------------------------
+        T[] items;
+        int count;
+        int capacity;
+        public int count
         {
-            // VARIABLES (HAS A...)
-            int count = 1;
-            int capacity = 4;
-                        
-            T[] items = new T[4];
-            T[] temp = new T[capacity * 2];
 
-            
+        }
 
-            // CONSTRUCTOR
-            public CustomList ()
+
+        
+
+
+
+        public T this[int index] // indexer properties
+        {
+            get
             {
-
-            }
-
-            // METHODS (CAN DO...)
-            for (int i = 0; i < count; i++)
-            {
-                temp[i] = items[i];
-                count++;
-                if (i == count || i == capacity) ;
+                if (index < 0 && index >= capacity)
                 {
-                    continue;
+                    return items[i]
                 }
             }
+            set
+            {
 
+            }
+        }
+
+
+        // ----------------------------------------------------------------------------------------------------------CONSTRUCTOR-----------------------------------------------------------------------------
+        public CustomList()
+        {
+            int count = 0;
+            int capacity = 4;
+
+            T[] items = new T[4];
+        }
+
+        // ----------------------------------------------------------------------------------------------------METHODS (CAN DO...)------------------------------------------------------------------------------
+        public void Add(T item)
+        {
+            if(count == capacity)
+            {
+                T[] temp = new T[capacity *= 2];
+                for (int i = 0; i < count; i++)
+                {
+                    temp[i] = items[i];
+                }
+                items = temp;
+            }
+            items[count] = item;
+            count++;
         }
     }
 }
